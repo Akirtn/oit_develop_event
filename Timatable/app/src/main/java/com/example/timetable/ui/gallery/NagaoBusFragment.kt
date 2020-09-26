f99bbadc93c7b365eb49f38d3c98c3c79d043bd3
aoken_7 <aokensabu1@gmail.com>
2020-09-27 05:13:02 +0900

    fix #21

---
+++ Timatable/app/src/main/java/com/example/timetable/ui/gallery/NagaoBusFragment.kt
@@ -0,0 +1,26 @@
+package com.example.timetable.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.timetable.R
+
+class NagaoBusFragment : Fragment() {
+
+  private lateinit var nagaoBusViewModel: NagaoBusViewModel
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    nagaoBusViewModel =
+    ViewModelProviders.of(this).get(NagaoBusViewModel::class.java)
+    val root = inflater.inflate(R.layout.fragment_nagao_bus, container, false)
+
+    return root
+  }
+}
