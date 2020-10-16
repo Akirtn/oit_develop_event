3849f0a42db57f7ba9696a7e559faf693f57448b
aoken7 <aokensabu1@gmail.com>
2020-10-16 22:35:06 +0900

    fix #49

--- Timatable/app/src/main/java/com/example/timetable/ui/kitayamaBus/KitayamaBusFragment.kt
+++ Timatable/app/src/main/java/com/example/timetable/ui/kitayamaBus/KitayamaBusFragment.kt
@@ -1,9 +1,12 @@
 package com.example.timetable.ui.kitayamaBus
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.timetable.R
@@ -20,6 +23,22 @@ class KitayamaBusFragment : Fragment() {
     kitayamaBusViewModel =
     ViewModelProviders.of(this).get(KitayamaBusViewModel::class.java)
     val root = inflater.inflate(R.layout.fragment_kitayama_bus, container, false)
+
+    val webView = root.findViewById<WebView>(R.id.kitayamaWebView)
+    webView.webViewClient = WebViewClient();
+    webView.settings.javaScriptEnabled = true
+
+    val progressDialog = ProgressDialog(root.context);
+    progressDialog.setMessage("Loading...");
+    progressDialog.show();
+
+    webView.loadUrl("https://busnavi.keihanbus.jp/pc/diagrampoledtl?mode=1&fr=%E5%8C%97%E5%B1%B1%E4%B8%AD%E5%A4%AE&frsk=B&tosk=&dt=202010162158&dd=2&dgmpl=%E5%8C%97%E5%B1%B1%E4%B8%AD%E5%A4%AE%E3%80%94%E4%BA%AC%E9%98%AA%E3%83%90%E3%82%B9%E3%80%95:2::41019,41029,40619,40629,40719,40729&p=0%2C8%2C10%2C12")
+
+    webView.webViewClient = object : WebViewClient() {
+      override fun onPageFinished(view: WebView, url: String) {
+        progressDialog.dismiss()
+      }
+    }
 
     return root
   }
